7bf926eb0301a8e6c6deeaecd58a2857f7be8a71
abhisin98 <abhisinha151@gmail.com>
2021-04-25 16:55:47 +0530

    update index.js

--- js/index.js
+++ js/index.js
@@ -7,7 +7,10 @@ var player;
 
 function onYouTubePlayerAPIReady(eid, vid) {
   player = new YT.Player(eid, {
-    videoId: youtube_parser(vid)
+    videoId: youtube_parser(vid),
+    events: {
+      'onStateChange': onPlayerStateChange
+    }
   });
 
   function youtube_parser(url) {
@@ -15,6 +18,27 @@ function onYouTubePlayerAPIReady(eid, vid) {
     var match = url.match(regExp);
     return (match && match[7].length == 11) ? match[7]: false;
   }
+
+}
+let mytimer
+function onPlayerStateChange(events) {
+
+  section3PreviewLyrics();
+  /*if (player.getPlayerState() == YT.PlayerState.PLAYING && section3ItemRun) {
+    /*mytimer = setInterval(function() {
+    }, 0);*/
+  /* } else {
+    //clearTimeout(mytimer);
+  }*/
+
+  /*****/
+  /*if (player.getPlayerState() == YT.PlayerState.PLAYING) {
+    mytimer = setInterval(function() {
+      $("#section1lyricsInput").val(player.playerInfo.currentTime);
+    }, 0);
+  } else {
+    clearTimeout(mytimer);
+  }*/
 }
 
 /********************************************************************************************* */
@@ -39,17 +63,27 @@ $(document).ready(function() {
   });
 
 
-  let lyrics
+  let lyrics = [];
 
   $("#section1Button").click(function() {
     $("#section1").hide();
     $("#section2").show();
+    player.pauseVideo();
 
     onYouTubePlayerAPIReady("section2Iframe", url);
 
-    lyrics = $("#section1lyricsInput").val().split("\n").filter(function(el) {
+    $("#section1lyricsInput").val().split("\n").filter(function(el) {
       return el;
-    });
+    }).forEach(lyricsToArray);
+
+    function lyricsToArray(item) {
+      lyrics.push({
+        id: create_UUID(),
+        lyrics: item,
+        startTime: "",
+        endTime: "",
+      });
+    }
     console.log(lyrics)
     repiter(lyrics, "#section2Repiter3", "h5 text-secondary");
 
@@ -58,9 +92,14 @@ $(document).ready(function() {
   /**************** Section  2 ************ */
   $("#section2SpeedIncrease").click(function() {
     //Video PlaybackRate  increase
+    let speed = player.getPlaybackRate() + 0.25;
+    $('#section2Speedtext').html(speed);
+    player.setPlaybackRate(speed);
   });
   $("#section2SpeedDecrease").click(function() {
-    //Video PlaybackRate  Decrease
+    let speed = player.getPlaybackRate() - 0.25;
+    $('#section2Speedtext').html(speed);
+    player.setPlaybackRate(speed);
   });
 
   let lyrics3 = [];
@@ -68,15 +107,12 @@ $(document).ready(function() {
   let status = false;
 
   $("#section2Button").click(function() {
-    //player.playVideo();
-    //console.log (player.playerInfo.currentTime, player)
-    //console.log(timeX(player.playerInfo.currentTime))
+    if (status) {
+      player.pauseVideo();
+      section3LyricsPrevew(lyrics3);
 
-    //console.log ('abhi', player.getPlayerState())
-
-    if (player.getPlayerState() == YT.PlayerState.PLAYING) {
-
-      if (status) {} else {
+    } else {
+      if (player.getPlayerState() == YT.PlayerState.PLAYING) {
 
         let elmnt = document.getElementById("section2Repiter2");
         elmnt.scrollIntoView({
@@ -89,30 +125,36 @@ $(document).ready(function() {
           console.log("start");
 
           lyrics2.push(lyrics.shift());
-          srtStart(lyrics2[0], player.playerInfo.currentTime);
+          // srtStart(lyrics2[0], player.playerInfo.currentTime);
+          lyrics2[0].startTime = player.playerInfo.currentTime;
           statusBack = false;
         } else if (lyrics.length < 1) {
           console.log("end");
 
-          srtEnd(player.playerInfo.currentTime)
+          lyrics2[0].endTime = player.playerInfo.currentTime;
+
+          //srtEnd(player.playerInfo.currentTime)
           lyrics3.push(lyrics2.shift());
           status = true;
-          console.log(makeSrt)
+          console.log(lyrics3)
         } else {
           console.log("default");
+          lyrics2[0].endTime = player.playerInfo.currentTime;
 
-          srtEnd(player.playerInfo.currentTime)
+          //srtEnd(player.playerInfo.currentTime)
           lyrics3.push(lyrics2.shift());
           lyrics2.push(lyrics.shift());
-          srtStart(lyrics2[0], player.playerInfo.currentTime)
+          //srtStart(lyrics2[0], player.playerInfo.currentTime)
+          lyrics2[0].startTime = player.playerInfo.currentTime;
         }
         repiter(lyrics, "#section2Repiter3", "h5 text-secondary");
         repiter(lyrics2, "#section2Repiter2", "h3");
         repiter(lyrics3, "#section2Repiter1", "h5 text-secondary");
         //console.log(lyrics, lyrics2, lyrics3)
+
+      } else {
+        player.playVideo();
       }
-    } else {
-      player.playVideo();
     }
   });
 
@@ -122,48 +164,103 @@ $(document).ready(function() {
 
     //Lyrics Sync Back
 
-    /*if (statusBack) {} else {
+    if (statusBack) {} else {
 
-                if (lyrics2.length < 1) {
-                    console.log("start back");
+      if (lyrics2.length < 1) {
+        console.log("start back");
 
-                    lyrics2.unshify(lyrics3.pop());
-                    //srtStart(lyrics2[0], player.playerInfo.currentTime)
-                } else if (lyrics3.length < 1) {
-                    console.log("end");
+        lyrics2.unshify(lyrics3.pop());
+        //srtStart(lyrics2[0], player.playerInfo.currentTime)
+        player.seekTo(lyrics2[0].startTime);
+      } else if (lyrics3.length < 1) {
+        console.log("end back");
 
-                    //srtEnd(player.playerInfo.currentTime)
-                    lyrics.unshift(lyrics2.pop());
-                    status = false;
-                    statusBack = true;
-                    //console.log(makeSrt)
-                } else {
-                    console.log("default");
+        //srtEnd(player.playerInfo.currentTime)
+        lyrics.unshift(lyrics2.pop());
+        status = false;
+        statusBack = true;
+        player.seekTo(lyrics[0].startTime);
+        //console.log(makeSrt)
+      } else {
+        console.log("default back");
 
-                   // srtEnd(player.playerInfo.currentTime)
-                    lyrics.unshift(lyrics2.pop());
-                    lyrics2.unshift(lyrics3.pop());
-                    //srtStart(lyrics2[0], player.playerInfo.currentTime)
-                }
-                repiter(lyrics, "#section2Repiter3", "h5 text-secondary");
-                repiter(lyrics2, "#section2Repiter2", "h3");
-                repiter(lyrics3, "#section2Repiter1", "h5 text-secondary");
-                //console.log(lyrics, lyrics2, lyrics3)
-            }*/
+        // srtEnd(player.playerInfo.currentTime)
+        lyrics.unshift(lyrics2.pop());
+        lyrics2.unshift(lyrics3.pop());
+        //srtStart(lyrics2[0], player.playerInfo.currentTime)
+        player.seekTo(lyrics2[0].startTime);
+      }
+      repiter(lyrics, "#section2Repiter3", "h5 text-secondary");
+      repiter(lyrics2, "#section2Repiter2", "h3");
+      repiter(lyrics3, "#section2Repiter1", "h5 text-secondary");
+      //console.log(lyrics, lyrics2, lyrics3)
+    }
   });
 
   /**************** Section  3 ************ */
+  function section3LyricsPrevew(item) {
+    //player.playVideo();
+    previewItem = item;
+    section3ItemRun = true;
+    $("#section2").hide();
+    $("#section3").show();
+    onYouTubePlayerAPIReady("section3Iframe",
+      url);
+  }
+
   $("#section3Button").click(function() {
 
     // Download Srt file
   });
 });
 
+/******************************************/
+let previewItem
+let section3ItemRun = false;
+let play_subtitles
+function section3PreviewLyrics() {
+  // Bind the play and pause methods
+  //this.on('play', function() {
+
+  if (player.getPlayerState() == YT.PlayerState.PLAYING && section3ItemRun) {
+    console.log(previewItem, "item")
+    play_subtitles = setInterval(function() {
+      var current_subtitle = 0;
+      if (player.playerInfo.currentTime > previewItem[current_subtitle].endTime) {
+        $("#section3PreviewLyrics").empty();
+      }
+
+      $.each(previewItem, function(index, subtitle) {
+        if (subtitle.startTime > player.playerInfo.currentTime)
+          return false;
+        else if (subtitle.startTime < player.playerInfo.currentTime && subtitle.endTime >= player.playerInfo.currentTime) {
+          if (index == current_subtitle) {
+            return false;
+          } else {
+            current_subtitle = index;
+            $("#section3PreviewLyrics").empty();
+            let html = `<p class="h3 text-white">${subtitle.lyrics}</p>`;
+            $("#section3PreviewLyrics").html(html);
+            /*$.each(subtitle, function(index, value) {
+              console.log(value, subtitle);
+              //$("#section3PreviewLyrics").append(value + "<br />");
+            });*/
+          }
+        }
+      });
+    }, 100);
+  } else {
+    clearTimeout(play_subtitles);
+  }
+}
+/*******************************/
+
+
 /**************** Function ************ */
 function repiter(lyrics, repiterName, h) {
   let html = "";
   for (var i = 0; i < lyrics.length; i++) {
-    html += `<p class="${h}">${lyrics[i]}</p>`
+    html += `<p class="${h}">${lyrics[i].lyrics}</p>`
   }
   $(repiterName).html(html);
 }
@@ -191,7 +288,7 @@ function timeX(timeInSeconds) {
   return pad(hours, 2) + ':' + pad(minutes, 2) + ':' + pad(seconds, 2) + ',' + pad(milliseconds, 3);
 }
 
-let makeSrt = [];
+/*let makeSrt = [];
 let tempSrt
 
 function srtStart(lyrics, startTime) {
@@ -206,7 +303,7 @@ function srtStart(lyrics, startTime) {
 function srtEnd(endTime) {
   tempSrt.endTime = endTime;
   makeSrt.push(tempSrt);
-}
+}*/
 
 
 
